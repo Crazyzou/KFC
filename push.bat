2089fff0b57269203d46de9bc4b8dc5496814770
crazyzou <2937718557@qq.com>
2026-06-23 16:53:34 +0800

    update 2026/06/23 周二 16:53:34.76

---
+++ push.bat
@@ -0,0 +1,6 @@
+@echo off 
+cd /d "C:\Users\Tower\Documents\π§æﬂœ‰"
+git add .
+git commit -m "update %date% %time%"
+git push origin main 
+pause 
--- push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git add .
-git commit -m "快速更新: $(date '+%Y-%m-%d %H:%M:%S')" || echo "没有新内容"
-git push origin main
